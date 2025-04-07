06e51c7699cc4f9b2d1008b59ad1b3fa2033b976
Zurokin <zurolina@yandex.ru>
2025-04-07 18:05:19 +0300

    sss

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: "licenses.txt", to: "" },
-        { from: "/images", to: "images" },
+        { from: "images", to: "" },
       ],
     }),
   ],
