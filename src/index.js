501143bc021032e515ecff3d7023c1a514f7dbc3
Zurokin <zurolina@yandex.ru>
2025-04-16 19:48:14 +0300

    ыы

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "../styles/style.css";
-import getCardType from "./cardType";
-
-document.querySelector("#submit-button").addEventListener("click", () => {
-  const cardNumber = document.querySelector("#card-number").value;
-  const cardType = getCardType(cardNumber);
-  alert(`Card type: ${cardType}`);
-});
